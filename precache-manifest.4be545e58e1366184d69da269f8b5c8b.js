49d25fdf8ef88055ff37bc7d4b37821826918f66
Serhiy Antipov <serhiy.antipov@gmail.com>
2020-07-30 11:27:16 +0300

    Updates

---
+++ precache-manifest.4be545e58e1366184d69da269f8b5c8b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd9cd3b265aee13c542f183aefb5814",
+    "url": "/SerhiyAntipov/site_news/index.html"
+  },
+  {
+    "revision": "931671a297fe242656b1",
+    "url": "/SerhiyAntipov/site_news/static/css/main.3d5dc2e6.chunk.css"
+  },
+  {
+    "revision": "325ca389ee4850b88f11",
+    "url": "/SerhiyAntipov/site_news/static/js/2.de2ca74e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SerhiyAntipov/site_news/static/js/2.de2ca74e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "931671a297fe242656b1",
+    "url": "/SerhiyAntipov/site_news/static/js/main.42e679ee.chunk.js"
+  },
+  {
+    "revision": "3f6389e586fab04c5d5f",
+    "url": "/SerhiyAntipov/site_news/static/js/runtime-main.6fa66bdc.js"
+  },
+  {
+    "revision": "c4d863251ca376e57e46e999f01b8ca4",
+    "url": "/SerhiyAntipov/site_news/static/media/logo.c4d86325.jpg"
+  },
+  {
+    "revision": "bcfca885ecefc1d461fa8685acf80c67",
+    "url": "/SerhiyAntipov/site_news/static/media/unnamed_01.bcfca885.jpg"
+  },
+  {
+    "revision": "55a08fa91d9b009ac56e7399afbadd17",
+    "url": "/SerhiyAntipov/site_news/static/media/user.55a08fa9.png"
+  }
+]);
