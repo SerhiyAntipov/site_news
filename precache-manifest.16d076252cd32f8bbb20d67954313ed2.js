3219ced5de6ed12cca9907c41b5101c0d04d79fd
Serhiy Antipov <serhiy.antipov@gmail.com>
2020-07-31 10:29:22 +0300

    Updates

--- precache-manifest.16d076252cd32f8bbb20d67954313ed2.js
+++ precache-manifest.16d076252cd32f8bbb20d67954313ed2.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0a343977abf889917e684712cf2801e3",
+    "revision": "d48ce91b7987ec45496042414fdef372",
     "url": "/site_news/index.html"
   },
   {
-    "revision": "011f1d7ef228d3e5188d",
-    "url": "/site_news/static/css/main.78c1f384.chunk.css"
+    "revision": "4bfcd42ceba0e6b2751f",
+    "url": "/site_news/static/css/main.fd34d793.chunk.css"
   },
   {
     "revision": "a38fc4fe0cf494541610",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/site_news/static/js/2.3ddf5d57.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "011f1d7ef228d3e5188d",
-    "url": "/site_news/static/js/main.daa3d22b.chunk.js"
+    "revision": "4bfcd42ceba0e6b2751f",
+    "url": "/site_news/static/js/main.c51a1296.chunk.js"
   },
   {
     "revision": "dd3e30285aaa21f09ae7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/site_news/precache-manifest.7608c24f6c970e44f06bb2076489273c.js"
+  "/site_news/precache-manifest.16d076252cd32f8bbb20d67954313ed2.js"
 );
 
 self.addEventListener('message', (event) => {
